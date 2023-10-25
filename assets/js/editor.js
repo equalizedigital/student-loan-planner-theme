d7cdbb030ea432b511dfe910eaafcfdcf4376f4a
Steve Jones <steve@stevejonesdev.com>
2023-10-25 14:49:19 -0400

    added - Event listener to dynamically update the background image of a preview container #562

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -76,3 +76,49 @@ function eqdReorderBlockCategories(customSlug) {
 // Example usage: prioritize the 'custom-blocks' category
 eqdReorderBlockCategories('custom-blocks');
 
+/**
+ * Event listener to dynamically update the background image of a preview container 
+ * in the block editor based on the mouseover event on block items.
+ *
+ * This provides a dynamic preview mechanism in the block editor based on the block the user is hovering over.
+ */
+document.addEventListener("mouseover", function(e) {
+	// Selector to preview block where you want to show background image.
+	const previewContainer = document.querySelector('.block-editor-inserter__preview-content-missing');
+
+	if (!previewContainer) {
+		return;
+	}
+
+	if (e.target.closest('.block-editor-block-types-list__item')) {
+		const hoveredBlock = e.target.closest('.block-editor-block-types-list__item');
+
+		// to find a name of the block we can extract it from block classes.
+
+		// Retrieve classes from the block on which the mouse is hovered.
+		const blockClasses = hoveredBlock.className.split(' ');
+
+		// Finding a class that starts with "editor-block-list-item-acf-".
+		const blockClass = blockClasses.find( cls => cls.startsWith("editor-block-list-item-acf-" ) );
+
+		// If such a class is found, extract the name from it.
+		if ( blockClass ) {
+			const blockName = blockClass.replace( "editor-block-list-item-acf-", "" );
+
+			// Get the image URL for this block
+			const imageUrl = wp.data.select( 'core/blocks' ).getBlockType( "acf/" + blockName )?.attributes?.previewImage?.default;
+
+			// adding our styles if there is a link to the picture.
+			if ( imageUrl ) {
+				previewContainer.style.background = `url(${imageUrl}) no-repeat center`;
+				previewContainer.style.backgroundSize = 'contain';
+				previewContainer.style.fontSize = '0px';
+			} else {
+				// remove our styles if there is no link.
+				previewContainer.style.background = '';
+				previewContainer.style.backgroundSize = '';
+				previewContainer.style.fontSize = '';
+			}
+		}
+	}
+});
