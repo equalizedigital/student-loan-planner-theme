ea4e4fd1553b87f5a5ccbe53747b648e2c68ec12
Steve Jones <steve@stevejonesdev.com>
2024-01-24 13:29:56 -0500

    updated - readability, and added proper escapting of outputs

--- template-parts/blocks/squared-image-content-callout-block/squared-image-content-callout.php
+++ template-parts/blocks/squared-image-content-callout-block/squared-image-content-callout.php
@@ -1,68 +1,61 @@
 <?php
-
 /**
- * squared-image-content-callout-block Block Template.
+ * Equalize Digital Base Theme file.
  *
- * @param  array  $block  The block settings and attributes.
- * @param  string  $content  The block inner HTML (empty).
- * @param  bool  $is_preview  True during AJAX preview.
- * @param  (int|string)  $post_id  The post ID this block is saved to.
+ * @package Equalize Digital Base Theme
  */
 
-if ( isset( $block['data']['preview_image_help'] ) ) :
+if ( isset( $block['data']['preview_image_help'] ) ) {
 	echo Loader_Gutenberg::get_preview_image( $block['data']['preview_image_help'], $block['name'] );
-
 	return;
-endif;
+}
 
 // Create id attribute allowing for custom 'anchor' value.
 $id = 'squared-image-content-callout-block-' . $block['id'];
-if ( ! empty( $block['anchor'] ) ) :
+if ( ! empty( $block['anchor'] ) ) {
 	$id = $block['anchor'];
-endif;
+}
 
 // Create class attribute allowing for custom 'className' and 'align' values.
 $className = 'block squared-image-content-callout-block';
-if ( ! empty( $block['className'] ) ) :
+if ( ! empty( $block['className'] ) ) {
 	$className .= ' ' . $block['className'];
-endif;
-if ( ! empty( $block['align'] ) ) :
+}
+if ( ! empty( $block['align'] ) ) {
 	$className .= ' align' . $block['align'];
-endif;
+}
 
 $className = apply_filters( 'loader_block_class', $className, $block, $post_id );
 
 // Load values and assing defaults.
 $title = get_field( 'title' );
-$copy = get_field( 'copy' );
+$copy  = get_field( 'copy' );
 if ( get_field( 'image' ) ) {
 	$image = get_field( 'image' );
 }
 $link = get_field( 'link' );
 ?>
 <section id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $className ); ?>">
-    <div class="squared-image-content-callout-container">
-        <figure class="squared-image-content-callout-container-image">
-			<?php if ( ! empty( $image ) ) : ?>
-				<?php echo wp_get_attachment_image( $image['id'], 'squared-image-content-callout-thumbnail' ); ?>
-			<?php endif; ?>
-        </figure>
+	<div class="squared-image-content-callout-container">
+		<figure class="squared-image-content-callout-container-image">
+			<?php 
+			if ( ! empty( $image ) ) {
+				echo wp_get_attachment_image( $image['id'], 'squared-image-content-callout-thumbnail' );
+			}
+			?>
+		</figure>
 
-        <div class="squared-image-content-callout-container-content">
-            <div>
-                <h2 class="title"><?php echo $title; ?></h2>
-				<?php
-				if ( ! empty( $link ) ) {
-					$url = $link['url'];
-					$title = $link['title'];
-					echo '<span class="content">';
-					echo "<a href=\"$url\" class=\"btn btn-dark-bg\">";
-					echo $title;
-					echo '</a>';
-					echo '</span>';
-				}
-				?>
-            </div>
-        </div>
-    </div>
+		<div class="squared-image-content-callout-container-content">
+			<div>
+				<h2 class="title"><?php echo $title; ?></h2>
+				<?php if ( ! empty( $link ) ) { ?>
+					<span class="content">
+						<a href="<?php echo esc_url( $link['url'] ); ?>" class="btn btn-dark-bg">
+							<?php echo esc_html( $link['title'] ); ?>
+						</a>
+					</span>
+				<?php } ?>
+			</div>
+		</div>
+	</div>
 </section>
