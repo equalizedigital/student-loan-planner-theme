2dfb480f24c84b455c3f8d32445d62ca396baa9e
nitya <tronsymphony@gmail.com>
2024-04-04 09:04:38 -0700

    PRICING

--- template-parts/blocks/pricing-calculator/template.php
+++ template-parts/blocks/pricing-calculator/template.php
@@ -36,112 +36,112 @@ $class_name         = apply_filters( 'loader_block_class', $class_name, $block, 
 ?>
 
 
-Straightforward pricing
-<section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
-	<div class="pricing_calculator_template_container">
-		<!--  -->
-		<div id="accordionGroup" class="pricing_calculator_accordion">
+	<section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>" aria-live="polite" aria-atomic="true" aria-relevant="additions text">
+		<div class="pricing_calculator_template_container">
+			<!--  -->
+			<div id="accordionGroup" class="pricing_calculator_accordion">
 
-		<?php if ( have_rows( 'services' ) ) : ?>
-			<?php while ( have_rows( 'services' ) ) :
-				the_row();
-				$row_index = get_row_index();
-				$title = get_sub_field( 'title' );
-				$description = get_sub_field( 'description' );
-				$video =	get_sub_field( 'video' );
-				$price = get_sub_field( 'price' );
-				$enrollment_fee = get_sub_field( 'enrollment_fee' );
-				$disclaimer = esc_html( get_sub_field( 'disclaimer' ) );
-				$benefits = get_sub_field( 'benefits' );
+			<?php if ( have_rows( 'services' ) ) : ?>
+				<?php while ( have_rows( 'services' ) ) :
+					the_row();
+					$row_index = get_row_index();
+					$title = get_sub_field( 'title' );
+					$description = get_sub_field( 'description' );
+					$video =	get_sub_field( 'video' );
+					$price = get_sub_field( 'price' );
+					$enrollment_fee = get_sub_field( 'enrollment_fee' );
+					$disclaimer = esc_html( get_sub_field( 'disclaimer' ) );
+					$benefits = get_sub_field( 'benefits' );
 
-				if($row_index === 1): ?>
-					<h3 class="screen-reader-text">Financial Planning</h3>
-					<div class="pricing_calculator_template_container_main">
-						<div class="pricing_calculator_template_container_main_pricing">
-							<div class="pricing_calculator_template_container_main_pricing_price">
-								<div class="large_set">$<span class="price"><?php  echo wp_kses_post($price); ?></span></div> <span>/month <sup>1</sup></span>
+					if($row_index === 1): ?>
+						<h3 class="screen-reader-text">Financial Planning</h3>
+						<div class="pricing_calculator_template_container_main">
+							<div class="pricing_calculator_template_container_main_pricing">
+								<div class="pricing_calculator_template_container_main_pricing_price">
+									<div class="large_set">$<span class="price"><?php  echo wp_kses_post($price); ?></span></div> <span>/month <sup>1</sup></span>
+								</div>
+								<div class="pricing_calculator_template_container_main_pricing_plus">+</div>
+								<div class="pricing_calculator_template_container_main_pricing_info">
+									<span class="info_set">$<span class="info_set_number"><?php  echo wp_kses_post($enrollment_fee); ?></span></span> enrollment fee
+								</div>
+								<div class="pricing_calculator_template_container_main_pricing_disclaimer"></div>
 							</div>
-							<div class="pricing_calculator_template_container_main_pricing_plus">+</div>
-							<div class="pricing_calculator_template_container_main_pricing_info">
-								<span class="info_set">$<span class="info_set_number"><?php  echo wp_kses_post($enrollment_fee); ?></span></span> enrollment fee
+							<div class="pricing_calculator_template_container_main_info">
+								<div class="pricing_calculator_template_container_main_info_title">At this pricing, you’re getting:</div>
+								<div class="pricing_calculator_template_container_main_info_ul">
+									<?php  echo wp_kses_post($benefits); ?>
+
+								</div>
+								<div class="pricing_calculator_template_container_main_info_started">
+									<a href="" class="btn btn-dark-bg">Get Started</a>
+								</div>
 							</div>
-							<div class="pricing_calculator_template_container_main_pricing_disclaimer"></div>
 						</div>
-						<div class="pricing_calculator_template_container_main_info">
-							<div class="pricing_calculator_template_container_main_info_title">At this pricing, you’re getting:</div>
-							<div class="pricing_calculator_template_container_main_info_ul">
-								<?php  echo wp_kses_post($benefits); ?>
+						<div class="heading">Add Additional Services:</div>
 
-							</div>
-							<div class="pricing_calculator_template_container_main_info_started">
-								<a href="" class="btn btn-dark-bg">Get Started</a>
-							</div>
-						</div>
-					</div>
-					<div class="heading">Add Additional Services:</div>
+				<?php else : ?>
 
-			<?php else : ?>
-
-			<div class="pricing_calculator_accordion_item">
-				<div class="pricing_calculator_accordion_item_title">
-					<button
-					class="pricing_calculator_accordion_add"
-					data-price="<?php echo wp_kses_post($price); ?>"
-					data-enrollment="<?php echo wp_kses_post($enrollment_fee); ?>"
-					data-disclaimer="<?php echo esc_attr($disclaimer); ?>"
-					data-benefits="<?php echo esc_attr($benefits); ?>"
-					data-added="false"
-					aria-pressed="false"
-					aria-label="add service">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/'; ?>add.svg" alt="add">
-					</button>
-					<button type="button" aria-expanded="true" class="pricing_calculator_accordion-trigger" aria-controls="sect<?php echo get_row_index(); ?>" id="pricing_calculator_accordion<?php echo get_row_index(); ?>">
-						<span class="pricing_calculator_accordion-title">
-							<?php  echo wp_kses_post($title); ?>
-							<span class="pricing_calculator_accordion-icon">
+				<div class="pricing_calculator_accordion_item">
+					<div class="pricing_calculator_accordion_item_title">
+						<button
+						class="pricing_calculator_accordion_add"
+						data-price="<?php echo wp_kses_post($price); ?>"
+						data-enrollment="<?php echo wp_kses_post($enrollment_fee); ?>"
+						data-disclaimer="<?php echo esc_attr($disclaimer); ?>"
+						data-benefits="<?php echo esc_attr($benefits); ?>"
+						data-added="false"
+						aria-pressed="false"
+						aria-label="add service">
+							<img src="<?php echo get_template_directory_uri() . '/assets/images/'; ?>add.svg" alt="add">
+						</button>
+						<button type="button" aria-expanded="true" class="pricing_calculator_accordion-trigger" aria-controls="sect<?php echo get_row_index(); ?>" id="pricing_calculator_accordion<?php echo get_row_index(); ?>">
+							<span class="pricing_calculator_accordion-title">
+								<?php  echo wp_kses_post($title); ?>
+								<span class="pricing_calculator_accordion-icon">
+								</span>
 							</span>
-						</span>
-					</button>
-				</div>
-				<div id="sect<?php echo get_row_index(); ?>" role="region" aria-labelledby="pricing_calculator_accordion<?php echo get_row_index(); ?>" class="pricing_calculator_accordion-panel">
-					<div class="pricing_calculator_accordion-panel_content">
-						<div class="pricing_calculator_accordion-panel_content_left">
-							<div class="title">
-								<?php echo wp_kses_post($title); ?>
-							</div>
-							<div class="content">
-								<?php  echo wp_kses_post($description); ?>
-							</div>
+						</button>
+					</div>
+					<div id="sect<?php echo get_row_index(); ?>" role="region" aria-labelledby="pricing_calculator_accordion<?php echo get_row_index(); ?>" class="pricing_calculator_accordion-panel">
+						<div class="pricing_calculator_accordion-panel_content">
+							<div class="pricing_calculator_accordion-panel_content_left">
+								<div class="title">
+									<?php echo wp_kses_post($title); ?>
+								</div>
+								<div class="content">
+									<?php  echo wp_kses_post($description); ?>
+								</div>
 
-							<button
-							class="action btn"
-							data-price="<?php echo wp_kses_post($price); ?>"
-							data-enrollment="<?php echo wp_kses_post($enrollment_fee); ?>"
-							data-disclaimer="<?php echo esc_attr($disclaimer); ?>"
-							data-benefits="<?php echo esc_attr($benefits); ?>"
-							data-added="false"
-							aria-pressed="false"
-							aria-label="add service">
-								Add Service
-							</button>
-						</div>
-						<div class="pricing_calculator_accordion-panel_content_right">
-							<?php
-							echo wp_kses_post($video); ?>
+								<button
+								class="action btn"
+								data-price="<?php echo wp_kses_post($price); ?>"
+								data-enrollment="<?php echo wp_kses_post($enrollment_fee); ?>"
+								data-disclaimer="<?php echo esc_attr($disclaimer); ?>"
+								data-benefits="<?php echo esc_attr($benefits); ?>"
+								data-added="false"
+								aria-pressed="false"
+								aria-label="add service">
+									Add Service
+								</button>
+							</div>
+							<div class="pricing_calculator_accordion-panel_content_right">
+								<?php
+								echo wp_kses_post($video); ?>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
+				<?php endif; ?>
+
+
+				<?php endwhile; ?>
 			<?php endif; ?>
-
-
-			<?php endwhile; ?>
-		<?php endif; ?>
+			</div>
+			<!--  -->
 		</div>
-		<!--  -->
-	</div>
-</section>
+	</section>
+
 
 
 <script>
@@ -253,13 +253,19 @@ function removeData($this) {
 jQuery('.action, .pricing_calculator_accordion_add').on('click', function() {
     var $this = jQuery(this);
     var dataAdded = $this.data('added');
+	var pricing_calculator_accordion_item = $this.closest('.pricing_calculator_accordion_item');
 
     if (dataAdded) {
         $this.data('added', false).attr('data-added', 'false');
+		jQuery(pricing_calculator_accordion_item).removeClass('pricing_calculator_accordion_add_active');
         removeData($this);
+
+		$this.closest('.pricing_calculator_accordion_item').find('.action').text('Add Service');
     } else {
         $this.data('added', true).attr('data-added', 'true');
         addData($this);
+		jQuery(pricing_calculator_accordion_item).addClass('pricing_calculator_accordion_add_active');
+		$this.closest('.pricing_calculator_accordion_item').find('.action').text('Remove Service');
     }
 
     // Toggle 'pressed' state for visual feedback or other uses
