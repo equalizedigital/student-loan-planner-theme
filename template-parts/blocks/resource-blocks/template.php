0449ed36eef647a52d8ddc275da49d55f283b9f8
nitya <tronsymphony@gmail.com>
2024-04-10 14:10:38 -0700

    resource block

--- template-parts/blocks/resource-blocks/template.php
+++ template-parts/blocks/resource-blocks/template.php
@@ -1,23 +1,23 @@
 <?php
-
 /**
- * resource blocks Block Template.
+ * Resource blocks Block Template.
  *
  * @param    array $block The block settings and attributes.
  * @param    string $content The block inner HTML (empty).
  * @param    bool $is_preview True during AJAX preview.
  * @param    (int|string) $post_id The post ID this block is saved to.
+ * @package resource block
  */
 
 if ( isset( $block['data']['preview_image_help'] ) ) :
-	echo Loader_Gutenberg::get_preview_image( $block['data']['preview_image_help'], $block['name'] );
+	echo esc_html( Loader_Gutenberg::get_preview_image( $block['data']['preview_image_help'], $block['name'] ) );
 	return;
 endif;
 
 // Create id attribute allowing for custom 'anchor' value.
-$id = 'resource-blocks-block-' . $block['id'];
+$block_id = 'resource-blocks-block-' . $block['id'];
 if ( ! empty( $block['anchor'] ) ) :
-	$id = $block['anchor'];
+	$block_id = $block['anchor'];
 endif;
 
 // Create class attribute allowing for custom 'className' and 'align' values.
@@ -33,40 +33,40 @@ endif;
 $class_name = apply_filters( 'loader_block_class', $class_name, $block, $post_id );
 
 // Load values and assing defaults.
-$title   = get_field( 'title' );
-$subcopy = get_field( 'subcopy' );
-$blocks  = get_field( 'blocks' );
+$title_field = get_field( 'title' );
+$subcopy     = get_field( 'subcopy' );
+$blocks      = get_field( 'blocks' );
 ?>
-<section id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
+<section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
 
 	<header class="resource-blocks-block-container">
-		<h2 class="title"><?php echo $title; ?></h2>
-		<span class="subtitle"><?php echo $subcopy; ?></span>
+		<h2 class="title"><?php echo esc_html( $title_field ); ?></h2>
+		<span class="subtitle"><?php echo esc_html( $subcopy ); ?></span>
 	</header>
 
 	<div class="resource-blocks-block-container-list">
-	<?php 
-		 
+	<?php
+
 	if ( $blocks ) {
 		echo '<ul class="resource-blocks-block-container-list-item">';
 		foreach ( $blocks as $row ) {
 			if ( ! empty( $row['link'] ) ) {
-				$link = $row['link']['url'];
+				$link_row = $row['link']['url'];
 			}
-				
+
 			if ( ! empty( $row['subcopy'] ) ) {
 				$subcopy = $row['subcopy'];
 			}
-			
+
 
 				echo '<li class="resource-blocks-block-container-list-item__block">';
-					echo "<a href=\"$link\">";
+					echo esc_html( "<a href=\"$link_row\">" );
 			if ( ! empty( $row['title'] ) ) {
-				$title = $row['title'];
+				$title_row = $row['title'];
 				?>
-							<h3 class="title"><?php _e( $title ); ?></h3>
+							<h3 class="title"><?php esc_html( $title_row ); ?></h3>
 						<?php } ?>
-							<p><?php _e( $subcopy ); ?></p>
+							<p><?php esc_html( $subcopy ); ?></p>
 						<?php
 						echo '</a>';
 						echo '</li>';
