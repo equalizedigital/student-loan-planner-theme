b8b6196de612def2829c399d7c9ef632299f0237
nitya <tronsymphony@gmail.com>
2024-04-12 12:14:36 -0700

    block

--- template-parts/blocks/columns-with-button/template.php
+++ template-parts/blocks/columns-with-button/template.php
@@ -41,7 +41,7 @@ $acf_rating_text = get_field( 'rating_text' );
 $acf_copy        = get_field( 'copy' );
 
 ?>
-<section id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
+<section id="<?php echo esc_attr( $classid ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
 	<div class="columns-with-button-block-container">
 		<div class="columns-with-button-block-container-content">
 			<h2 class="columns-with-button-block-container-content__title"><?php echo wp_kses_post( $acf_title ); ?></h2>
@@ -49,7 +49,7 @@ $acf_copy        = get_field( 'copy' );
 			<?php
 			echo wp_kses_post( $acf_link_url );
 			if ( isset( $_SERVER['QUERY_STRING'] ) && ! empty( $_SERVER['QUERY_STRING'] ) ) {
-				echo '&#63;' . $_SERVER['QUERY_STRING']; }
+				echo wp_kses_post( '&#63;' . $_SERVER['QUERY_STRING'] ); }
 			?>
 			" class="columns-with-button-block-container-content__button btn">
 				<?php echo wp_kses_post( $acf_link_title ); ?>
@@ -57,10 +57,10 @@ $acf_copy        = get_field( 'copy' );
 			<div class="columns-with-button-block-container-content__rating">
 				<?php
 				if ( ! empty( $acf_rating_text['url'] ) ) {
-					echo '<a href="' . $acf_rating_text['url'] . '">';
+					echo '<a href="' . esc_url( $acf_rating_text['url'] ) . '">';
 				}
 				?>
-				<?php echo $acf_rating_text['title']; ?>
+				<?php echo wp_kses_post( $acf_rating_text['title'] ); ?>
 				<?php
 				if ( ! empty( $acf_rating_text['url'] ) ) {
 					echo '</a>';
--- template-parts/blocks/team-hightlight/template.php
+++ template-parts/blocks/team-hightlight/template.php
@@ -44,7 +44,7 @@ endif;
 	<div class="team-hightlight-block-container">
 
 		<header class="team-hightlight-block-container-header">
-			<h2 class="team-hightlight-block-container-header__title"><?php echo esc_attr( $acf_title ); ?></h2>
+			<h2 class="team-hightlight-block-container-header__title"><?php echo esc_html( $acf_title ); ?></h2>
 		</header>
 
 		<ul class="team-hightlight-block-container-team-hightlight" id="team-hightlight-block-container-team-hightlight">
@@ -59,7 +59,6 @@ endif;
 					$member = get_sub_field( 'member' );
 					$author = get_field( 'post_author', $member->ID );
 					if ( ! is_null( $author->ID ) ) {
-						$author_id   = $author->ID;
 						$author_name = get_userdata( $author->ID );
 					}
 
@@ -77,9 +76,9 @@ endif;
 
 						<?php if ( ! empty( $acf_use_alternative_styling ) ) : ?>
 							<?php if ( ! is_null( $author->ID ) ) : ?>
-								<a href="<?php echo get_site_url() . '/author/' . $author_name->user_nicename; ?>" class="team-hightlight-block-container-team-hightlight-member__button">
+								<a href="<?php echo esc_url( get_site_url() . '/author/' . $author_name->user_nicename ); ?>" class="team-hightlight-block-container-team-hightlight-member__button">
 							<?php else : ?>
-								<button class="team-hightlight-block-container-team-hightlight-member__button modal-btn" data-modal="modal<?php echo get_row_index(); ?>-<?php echo esc_attr( $classid ); ?>" aria-label="Open Video">
+								<button class="team-hightlight-block-container-team-hightlight-member__button modal-btn" data-modal="modal<?php echo esc_html( get_row_index() ); ?>-<?php echo esc_html( $classid ); ?>" aria-label="Open Video">
 							<?php endif; ?>
 						<?php endif; ?>
 
@@ -101,7 +100,7 @@ endif;
 								<?php if ( ! empty( $member->ID ) ) : ?>
 								<span class="title"><?php echo wp_kses_post( get_the_title( $member->ID ) ); ?></span>
 								<?php endif; ?>
-								<span class="job"><?php the_field( 'job_title', $member->ID ); ?></span>
+								<span class="job"><?php echo wp_kses_post( get_field( 'job_title', $member->ID ) ); ?></span>
 							</div>
 
 							<?php if ( ! empty( $acf_use_alternative_styling ) ) : ?>
@@ -176,10 +175,10 @@ if ( have_rows( 'team' ) ) :
 							<?php echo wp_kses_post( $member->post_title ); ?>
 						</span>
 						<span class="job">
-							<?php the_field( 'job_title', $member->ID ); ?>
+							<?php echo wp_kses_post( get_field( 'job_title', $member->ID ) ); ?>
 						</span>
 						<div class="content">
-							<?php echo wpautop( wp_kses_post( $member->post_content ) ); ?>
+							<?php echo wp_kses_post( wpautop( wp_kses_post( $member->post_content ) ) ); ?>
 						</div>
 						<?php
 						if ( $booking_url ) {
