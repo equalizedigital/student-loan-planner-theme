3761001336000e5666b142d1b5cc57412ee44e35
nitya <tronsymphony@gmail.com>
2024-04-12 10:33:11 -0700

    block

--- template-parts/blocks/affiliant-landing-block/template.php
+++ template-parts/blocks/affiliant-landing-block/template.php
@@ -33,15 +33,15 @@ endif;
 $class_name = apply_filters( 'loader_block_class', $class_name, $block, $post_id );
 
 // Check if 'slug' is set in the URL parameters.
-if (isset($_GET['landing_page'])) {
-    $landing_page = sanitize_text_field($_GET['landing_page']);
+if ( isset( $_GET['landing_page'] ) ) {  //phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	$landing_page = sanitize_text_field( $_GET['landing_page'] );  //phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable, WordPress.Security.NonceVerification.Recommended
 
 	// Query for the page by slug.
-	$args = array(
+	$args       = array(
 		'post_type'   => 'slp_landing',
 		'post_status' => 'publish',
 		'numberposts' => 1,
-		'meta_query'  => array(
+		'meta_query'  => array(  //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
 			array(
 				'key'     => 'landing_page_url_text',
 				'value'   => $page_slug,
@@ -49,11 +49,11 @@ if (isset($_GET['landing_page'])) {
 			),
 		),
 	);
-	$page = get_posts( $args );
+	$page_posts = get_posts( $args );
 
 	// If the page exists, redirect or load the page.
-	if ( $page ) {
-		$page_id = $page[0]->ID;
+	if ( $page_posts ) {
+		$page_id = $page_posts[0]->ID;
 
 		$parameter_page = $page_id;
 	}
@@ -65,15 +65,14 @@ if ( ! isset( $parameter_page ) ) {
 <section id="<?php echo esc_attr( $blockid ); ?>" class="ed_landing_works_editor
 						<?php
 						if ( empty( $parameter_page ) ) {
-							echo 'ed_landing_works_editor_empty'; } else {
-							}
-							?>
+							echo 'ed_landing_works_editor_empty'; }
+						?>
 	<?php echo esc_attr( $class_name ); ?>">
 	<div class="ed_landing_works_editor_container">
 		<div class="ed_landing_works_editor_container_content">
-			<h2 class="heading"><?php the_field( 'heading' ); ?></h2>
+			<h2 class="heading"><?php echo esc_html( get_field( 'heading' ) ); ?></h2>
 			<?php
-				the_field( 'how_does_the_consult_work' );
+				echo esc_html( get_field( 'how_does_the_consult_work' ) );
 			?>
 		</div>
 
@@ -82,7 +81,7 @@ if ( ! isset( $parameter_page ) ) {
 		if ( get_field( 'how_does_the_consult_work_youtube_id' ) ) :
 			?>
 					<div class="ed_landing_works_editor_container_media">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/<?php the_field( 'how_does_the_consult_work_youtube_id' ); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo esc_html( get_field( 'how_does_the_consult_work_youtube_id' ) ); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 					</div>
 					<?php
 					endif;
